aaacfde71cccbb26d8eead083d3e3229d34e8dd1
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-06-24 21:15:24 +0200

    Refactor HTML, CSS, JS

--- app.js
+++ app.js
@@ -1,62 +1,65 @@
-const leftButton = document.querySelector('.slider__left-button');
-const rightButton = document.querySelector('.slider__right-button');
-const slider = document.querySelector('.slider');
-const caption = document.querySelector('.slider__caption');
+class Slider {
+  constructor() {
+    this.leftButton = document.querySelector(".slider__button--left");
+    this.rightButton = document.querySelector(".slider__button--right");
+    this.slider = document.querySelector(".slider");
+    this.caption = document.querySelector(".slider__caption");
 
-const img1 = "img/img1.jpg"
-const img2 = "img/img2.jpg"
-const img3 = "img/img3.jpg"
-const img4 = "img/img4.jpg"
-const img5 = "img/img5.jpg"
+    this.images = [
+      "img/img1.jpg",
+      "img/img2.jpg",
+      "img/img3.jpg",
+      "img/img4.jpg",
+      "img/img5.jpg",
+    ];
+    this.index = 0;
 
-const images = [img1, img2, img3, img4, img5]
-let index = 0;
-caption.innerText = `${index + 1}/${images.length}`
-leftButton.style.opacity = '0.5'
-leftButton.style.pointerEvents = 'none'
+    this.caption.innerText = `${this.index + 1}/${this.images.length}`;
+    this.leftButton.style.opacity = "0.5";
+    this.leftButton.style.pointerEvents = "none";
 
-const buttonBlock = () => {
-    if (index === 0) {
-        leftButton.style.opacity = '0.5'
-        leftButton.style.pointerEvents = 'none'
-    } else if (index === images.length - 1) {
-        rightButton.style.opacity = '0.5'
-        rightButton.style.pointerEvents = 'none'
-    } else {
-        leftButton.style.opacity = '1'
-        leftButton.style.pointerEvents = 'auto'
-        rightButton.style.opacity = '1'
-        rightButton.style.pointerEvents = 'auto'
+    this.leftButton.addEventListener("click", () => this.prevSlide());
+    this.rightButton.addEventListener("click", () => this.nextSlide());
+    document.addEventListener("keydown", (e) => this.handleKeyDown(e));
+  }
+
+  buttonBlock() {
+    const isLeftButtonDisabled = this.index === 0;
+    const isRightButtonDisabled = this.index === this.images.length - 1;
+
+    this.leftButton.style.opacity = isLeftButtonDisabled ? "0.5" : "1";
+    this.leftButton.style.pointerEvents = isLeftButtonDisabled
+      ? "none"
+      : "auto";
+    this.rightButton.style.opacity = isRightButtonDisabled ? "0.5" : "1";
+    this.rightButton.style.pointerEvents = isRightButtonDisabled
+      ? "none"
+      : "auto";
+  }
+
+  updateSlide() {
+    this.slider.style.backgroundImage = `url(${this.images[this.index]})`;
+    this.caption.innerText = `${this.index + 1}/${this.images.length}`;
+    this.buttonBlock();
+  }
+
+  prevSlide() {
+    this.index = Math.max(this.index - 1, 0);
+    this.updateSlide();
+  }
+
+  nextSlide() {
+    this.index = Math.min(this.index + 1, this.images.length - 1);
+    this.updateSlide();
+  }
+
+  handleKeyDown(e) {
+    if (e.key === "ArrowLeft") {
+      this.prevSlide();
+    } else if (e.key === "ArrowRight") {
+      this.nextSlide();
     }
+  }
 }
 
-const nextSlide = () => {
-    index++;
-    if (index > images.length - 1) {
-        index = images.length - 1
-    }
-
-    slider.style.backgroundImage = `url(${images[index]})`
-    caption.innerText = `${index + 1}/${images.length}`
-    buttonBlock()
-}
-
-const prevSlide = () => {
-    index--
-    if (index < 0) {
-        index = 0;
-    }
-    slider.style.backgroundImage = `url(${images[index]})`
-    caption.innerText = `${index + 1}/${images.length}`
-    buttonBlock()
-}
-
-rightButton.addEventListener('click', nextSlide)
-leftButton.addEventListener('click', prevSlide)
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowRight') {
-        nextSlide()
-    } else if (e.key === 'ArrowLeft') {
-        prevSlide()
-    }
-})
+const slider = new Slider();
